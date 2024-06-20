e1f99411953cd62ed3c18aad9055b126145b0bd1
derrick921213 <dlin12457@gmail.com>
2024-06-20 13:17:42 +0800

    fix: 修正以符合用途

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,31 +6,19 @@ const config: Config = {
   title: 'Point System',
   tagline: 'A point system for our Project',
   favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
   url: 'https://project.duacodie.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Duacodie', // Usually your GitHub org/user name.
-  projectName: 'PointSystem', // Usually your repo name.
-
+  organizationName: 'derrick921213',
+  projectName: 'PointSystem',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'zh-TW',
     locales: ['en', 'zh-TW'],
     localeConfigs: {
       en: {
         label: 'English',
-        direction: 'ltr',       // 閱讀方向為左到右
+        direction: 'ltr',
       },
       'zh-TW': {
         label: '繁體中文（台灣）',
@@ -45,17 +33,8 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/derrick921213/PointSystem/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -68,7 +47,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'PointSystem',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
@@ -80,17 +59,17 @@ const config: Config = {
           position: 'left',
           label: 'Tutorial',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
+        // {to: '/blog', label: 'Blog', position: 'left'},
+        // {
+        //   href: 'https://github.com/facebook/docusaurus',
+        //   label: 'GitHub',
+        //   position: 'right',
+        // },
       ],
     },
     footer: {
       style: 'dark',
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Duacodie inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
